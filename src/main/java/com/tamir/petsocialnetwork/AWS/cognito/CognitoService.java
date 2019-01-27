ea71a9b3ec0e10000c1a9e760f48c3377f825fa6
tamiryup <tamirishaysh@gmail.com>
2019-01-27 11:58:52 +0200

    Remove unused function from CognitoService

--- src/main/java/com/tamir/petsocialnetwork/AWS/cognito/CognitoService.java
+++ src/main/java/com/tamir/petsocialnetwork/AWS/cognito/CognitoService.java
@@ -133,21 +133,4 @@ public class CognitoService {
         return result;
     }
 
-    public static String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
-        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
-
-        SecretKeySpec signingKey = new SecretKeySpec(
-                userPoolClientSecret.getBytes(StandardCharsets.UTF_8),
-                HMAC_SHA256_ALGORITHM);
-        try {
-            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
-            mac.init(signingKey);
-            mac.update(userName.getBytes(StandardCharsets.UTF_8));
-            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
-            return Base64.getEncoder().encodeToString(rawHmac);
-        } catch (Exception e) {
-            throw new RuntimeException("Error while calculating ");
-        }
-    }
-
 }
