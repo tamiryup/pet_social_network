045994ba1cd42d6a770cbed68fd0123b04e64642
tamiryup <tamirishaysh@gmail.com>
2019-02-01 22:59:52 +0200

    Catch Exceptions from filter

---
+++ src/main/java/com/tamir/petsocialnetwork/ErrorControllerImpl.java
@@ -0,0 +1,23 @@
+package com.tamir.petsocialnetwork;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorControllerImpl implements ErrorController {
+
+    @RequestMapping("/error")
+    public void handleError(HttpServletRequest request) throws Throwable {
+        if (request.getAttribute("javax.servlet.error.exception") != null) {
+            throw (Throwable) request.getAttribute("javax.servlet.error.exception");
+        }
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
