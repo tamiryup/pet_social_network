eb841ca674367c12e18334244b2bfccce966a5bc
tamiryup <tamirishaysh@gmail.com>
2019-09-14 16:42:00 +0300

    Add store service

---
+++ src/main/java/com/tamir/followear/services/StoreService.java
@@ -0,0 +1,33 @@
+package com.tamir.followear.services;
+
+import com.google.common.collect.Lists;
+import com.tamir.followear.entities.Store;
+import com.tamir.followear.repositories.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class StoreService {
+
+    @Autowired
+    StoreRepository storeRepo;
+
+    public Store create(Store store) {
+        return storeRepo.save(store);
+    }
+
+    public Map<Long, Store> makeMapFromIds(List<Long> ids) {
+        List<Store> storeList = Lists.newArrayList(storeRepo.findAllById(ids));
+
+        Map<Long, Store> storeMap = storeList.stream()
+                .collect(Collectors.toMap(store -> store.getId(), store -> store));
+
+        return storeMap;
+    }
+}
