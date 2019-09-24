091892f4de1c0bbf2948968b86b0651499ad7226
tamiryup <tamirishaysh@gmail.com>
2019-09-24 16:22:21 +0300

    Make relevantUsersCache

--- src/main/java/com/tamir/followear/services/ExploreService.java
+++ src/main/java/com/tamir/followear/services/ExploreService.java
@@ -1,9 +1,13 @@
 package com.tamir.followear.services;
 
 import com.google.common.base.Suppliers;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
@@ -11,16 +15,39 @@ import java.util.function.Supplier;
 @Service
 public class ExploreService {
 
-    private final int mostPopularUsersLimit = 50;
-
     @Autowired
     FollowService followService;
 
+    private final int mostPopularUsersLimit = 50;
+
+    private final int relevantUsersLimit = 50;
+
     //save 50 most popular users in cache and update once every day
-    private Supplier<List<Long>> popularUsers = Suppliers.memoizeWithExpiration(
-            this::mostPopularUsersSupplier, 1, TimeUnit.DAYS);
+    private Supplier<List<Long>> popularUsers;
+
+    //cache to save 50 relevant users for a specific user
+    private LoadingCache<Long, List<Long>> relevantUsersCache;
+
+    @PostConstruct
+    private void init() {
+        popularUsers = Suppliers.memoizeWithExpiration(
+                this::mostPopularUsersSupplier, 1, TimeUnit.DAYS);
+
+        relevantUsersCache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .build(new CacheLoader<Long, List<Long>>() {
+                    @Override
+                    public List<Long> load(Long userId) throws Exception {
+                        return loadRelevantUsers(userId);
+                    }
+                });
+    }
 
     private List<Long> mostPopularUsersSupplier() {
         return followService.mostPopularUsersIds(mostPopularUsersLimit);
+    }
+
+    private List<Long> loadRelevantUsers(long userId) {
+        return followService.relevantSuggestionsForUser(userId, relevantUsersLimit);
     }
 }
