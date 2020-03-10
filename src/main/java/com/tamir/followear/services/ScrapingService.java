8dfa819674e260e4feede0a7d0c1037d437f7eb2
Adi Caspi <adicaspi88@gmail.com>
2020-03-10 20:11:48 +0200

    Uncomment web driver options

--- src/main/java/com/tamir/followear/services/ScrapingService.java
+++ src/main/java/com/tamir/followear/services/ScrapingService.java
@@ -73,13 +73,13 @@ public class ScrapingService {
         ChromeOptions options = new ChromeOptions();
         options.setBinary(chromeBinary);
 
-//        options.addArguments("--headless", "--no-sandbox", "--disable-gpu", "--window-size=1280x1696",
-//                "--user-data-dir=/tmp/user-data", "--hide-scrollbars", "--enable-logging",
-//                "--log-level=0", "--v=99", "--single-process", "--data-path=/tmp/data-path",
-//                "--ignore-certificate-errors", "--homedir=/tmp", "--disk-cache-dir=/tmp/cache-dir",
-//                "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36" +
-//                        " (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
-//
+        options.addArguments("--headless", "--no-sandbox", "--disable-gpu", "--window-size=1280x1696",
+                "--user-data-dir=/tmp/user-data", "--hide-scrollbars", "--enable-logging",
+                "--log-level=0", "--v=99", "--single-process", "--data-path=/tmp/data-path",
+                "--ignore-certificate-errors", "--homedir=/tmp", "--disk-cache-dir=/tmp/cache-dir",
+                "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36" +
+                        " (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
+
         WebDriver driver = new ChromeDriver(options);
         return driver;
     }
